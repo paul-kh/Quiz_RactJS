ee5e7983bb18e3cd10eb601d80fe5aea386b0a28
Paul Chheang <phalcheat@gmail.com>
2024-06-22 12:12:15 -0500

    Displayed first question on Quiz component

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,7 +1,27 @@
+import { useState } from "react";
+
+import QUESTIONS from "../questions";
+
 export default function Quiz() {
+  /**********************************************************************************
+   * We shuffle answer choices for each question.
+   * Each question has timer that will run out
+   * If user didn't pick an answer, timer runs out, answer will be set to "NULL"
+   **********************************************************************************/
+
+  const [userAnswers, setUserAnswers] = useState([]);
+  const activeQuestionIndex = userAnswers.length;
+
   return (
-    <>
-      <h1>Some quizzes coming soon...</h1>
-    </>
+    <div id="question">
+      <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+      <ul id="answers">
+        {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+          <li key={answer} className="answer">
+            <button>{answer}</button>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
