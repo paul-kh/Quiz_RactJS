19f1709f4b9452c802667844bf66c81ac70c1184
Paul Chheang <phalcheat@gmail.com>
2024-06-22 11:44:13 -0500

    Added Alt attribute to <img> tag

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
-import quizImage from "../assets/quiz-logo.png";
+import quizLogo from "../assets/quiz-logo.png";
 
 export default function Header() {
   return (
     <header>
-      <img src={quizImage} />
+      <img src={quizLogo} alt="Quiz Logo" />
       <h1>ReactQuiz</h1>
     </header>
   );
