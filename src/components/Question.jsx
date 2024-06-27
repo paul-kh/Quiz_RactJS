992dd4cfd9289aec47c02d4f8a920855a7dd0547
Paul Chheang <phalcheat@gmail.com>
2024-06-26 23:17:45 -0500

    Moved some logics from <Quiz> to <Question>

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,23 +1,53 @@
+import { useState } from "react";
+
 import QuestionTimer from "./QuestionTimer";
 import Answers from "./Answers";
+import QUESTIONS from "../questions.js";
 
-export default function Question({
-  questionText,
-  answers,
-  selectedAnswer,
-  answerState,
-  onSelectAnswer,
-}) {
+export default function Question({ index, onSelectAnswer }) {
+  // Move state from Quiz component here
+  const [answer, setAnswer] = useState({
+    selectedAnswer: "",
+    isCorrect: null,
+  });
+
+  function handleSelectAnswer(answer) {
+    setAnswer({
+      selectedAnswer: answer,
+      isCorrect: null,
+    });
+
+    setTimeout(() => {
+      setAnswer({
+        selectedAnswer: answer,
+        isCorrect: QUESTIONS[index].answers[0] === answer,
+      });
+
+      setTimeout(() => {
+        onSelectAnswer(answer); // Passing selected answer to Quiz component via handleSelectAnswer(answer)
+      }, 2000);
+    }, 1000);
+  }
+
+  let answerState = "";
+  console.log("Selected Answer: ", answer.selectedAnswer);
+  console.log("isCorrect: ", answer.isCorrect);
+  if (answer.selectedAnswer && answer.isCorrect !== null) {
+    answerState = answer.isCorrect ? "correct" : "wrong";
+  } else if (answer.selectedAnswer) {
+    answerState = "answer";
+  }
+
   return (
     <div id="question">
       <QuestionTimer timeout={15000} onTimeout={() => onSelectAnswer(null)} />
 
-      <h2>{questionText}</h2>
+      <h2>{QUESTIONS[index].text}</h2>
       <Answers
-        answers={answers}
-        selectedAnswer={selectedAnswer}
+        answers={QUESTIONS[index].answers}
+        selectedAnswer={answer.selectedAnswer}
         answerState={answerState}
-        onSelect={onSelectAnswer}
+        onSelect={handleSelectAnswer}
       />
     </div>
   );
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -6,56 +6,19 @@ import quizCompletedImg from "../assets/quiz-complete.png";
 import Question from "./Question";
 
 export default function Quiz() {
-  /**********************************************************************************
-   * We shuffle answer choices for each question.
-   * Each question has timer that will run out
-   * If user didn't pick an answer, timer runs out, answer will be set to "NULL"
-   **********************************************************************************/
-
   const [userAnswers, setUserAnswers] = useState([]);
-
-  /***********************************************************************************
-   * We highlight the answer when user selected it.
-   * If the selected answer is correct => highlight in Green
-   * If the selected answer is wrong => hightlight in red
-   * After the answer is selected for 2 seconds => move to next question
-   ***********************************************************************************/
-  const [answerState, setAnswerState] = useState("");
-
-  // We need to pause question not to move to next question if no answer has been selected yet
-  // If no answer selected yet, the answerState = "" though we set activeQuestionIndex to
-  // the length of the userAnswers array
-  const activeQuestionIndex =
-    answerState === "" ? userAnswers.length : userAnswers.length - 1;
-  console.log("activeQuestionIndex: ", activeQuestionIndex);
-
   console.log("userAnswers: ", userAnswers);
 
-  function handleSelectAnswer(selectedAnswer) {
-    setAnswerState("answered"); // when user selected an answer
+  const activeQuestionIndex = userAnswers.length;
+  console.log("activeQuestionIndex: ", activeQuestionIndex);
 
+  function handleSelectAnswer(selectedAnswer) {
     setUserAnswers((prevSelectedAnswers) => {
       return [...prevSelectedAnswers, selectedAnswer];
     });
-
-    // After 1 second, we highlight answer based on correct or wrong answer
-    // Note: The first answer in the answer array of the original QUESTIONS array
-    //       is the correct answer.
-    setTimeout(() => {
-      if (selectedAnswer === QUESTIONS[activeQuestionIndex].answers[0]) {
-        setAnswerState("correct");
-      } else {
-        setAnswerState("wrong");
-      }
-
-      // Move to next question 2 seconds after an answer was selected
-      setTimeout(() => {
-        setAnswerState("");
-      }, 2000);
-    }, 1000);
   }
 
-  if (activeQuestionIndex >= QUESTIONS.length) {
+  if (activeQuestionIndex === QUESTIONS.length) {
     return (
       <div id="summary">
         <img src={quizCompletedImg} alt="Trophy icon" />
@@ -68,10 +31,7 @@ export default function Quiz() {
     <div id="quiz">
       <Question
         key={activeQuestionIndex} // 'key' prop here is for forcing React to destroy old component and remout new instance
-        questionText={QUESTIONS[activeQuestionIndex].text}
-        answers={QUESTIONS[activeQuestionIndex].answers}
-        answerState={answerState}
-        selectedAnswer={userAnswers[userAnswers.length - 1]}
+        index={activeQuestionIndex}
         onSelectAnswer={handleSelectAnswer}
       />
     </div>
